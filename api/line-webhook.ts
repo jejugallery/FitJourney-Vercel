6421717bc08400304d7584500151e20bcc2393ee
jejugallery <jmaster.ani@gmail.com>
2026-07-14 23:20:00 +0700

    Revert "style: change login banner aspectRatio to standard 5:3"
    This reverts commit b9ad7773179821c7c9af696dde89db8f95c81ea6.

--- api/line-webhook.ts
+++ api/line-webhook.ts
@@ -178,7 +178,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
               type: 'image',
               url: 'https://i.postimg.cc/QMzCQFzk/login.png',
               size: 'full',
-              aspectRatio: '5:3',
+              aspectRatio: '1000:618',
               aspectMode: 'cover',
               action: {
                 type: 'uri',
